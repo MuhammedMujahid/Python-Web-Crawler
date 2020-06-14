93f256026cd05a29e9921b7d5ab0e38a6e542707
Muhammed <muhammedmujahid95@gmail.com>
2020-06-14 16:14:55 -0400

    Search bar automation test

--- scraper.py
+++ scraper.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Kceys
+import time
+
 PATH = '/home/muhammed/Downloads/chromedriver_linux64/chromedriver'
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://www.businessesforsale.com")
 print(driver.title)
+
+search = driver.find_element_by_name("Keywords")
+search.send_keys("test")
+search.send_keys(Keys.RETURN) #hits enter
+
+time.sleep(5)
+
 driver.quit()
